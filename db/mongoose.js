9b8a99f62a51cfdd2dd28bca2a0e756fb41027d7
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2020-10-22 00:01:16 +0500

    Closes #14 Added express-validator and mongoose-unique-validator to userRoutes

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,36 +1,48 @@
-const mongoose = require('mongoose')
-const schemas = require("./schemas")
-const Bcrypt = require("bcryptjs")
+const mongoose = require('mongoose');
+const schemas = require('./schemas');
+const Bcrypt = require('bcryptjs');
+const uniqueValidator = require('mongoose-unique-validator');
 
-const mongoURI = 'mongodb://127.0.0.1:27017/library'
+const mongoURI = 'mongodb://127.0.0.1:27017/library';
 
 const optionsMoongoose = {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-}
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+};
 
-mongoose.connection.on('error', err => console.error(err))
-mongoose.connection.once('open', () => console.log("connection established to mongodb"))
-mongoose.connect(mongoURI, optionsMoongoose)
+mongoose.connection.on('error', (err) => console.error(err));
+mongoose.connection.once('open', () =>
+  console.log('connection established to mongodb')
+);
+mongoose.connect(mongoURI, optionsMoongoose);
 
-
-const userSchema = mongoose.Schema(schemas.user)
-userSchema.pre("save", function (next) {
-    if (!this.isModified("password")) {
-        return next()
-    }
-    this.password = Bcrypt.hashSync(this.password, 10)
-    next()
+const userSchema = mongoose.Schema(schemas.user);
+userSchema.plugin(uniqueValidator);
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  this.password = Bcrypt.hashSync(this.password, 10);
+  next();
 });
-const userDoc = mongoose.model('user', userSchema)
 
-const bookSchema = mongoose.Schema(schemas.book)
-const bookDoc = mongoose.model('book', bookSchema)
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.password;
+  },
+});
+
+const userDoc = mongoose.model('user', userSchema);
+
+const bookSchema = mongoose.Schema(schemas.book);
+const bookDoc = mongoose.model('book', bookSchema);
 
 module.exports = {
-    db: mongoose,
-    bookDoc,
-    userDoc,
-}
+  db: mongoose,
+  bookDoc,
+  userDoc,
+};
--- routers/books.js
+++ routers/books.js
@@ -1,30 +1,34 @@
+const bookRouter = require('express').Router();
+
 /** get all books. */
-app.get('/', (req, res) => {
-    console.log("GET")
-    res.send('Hello World!')
-})
+bookRouter.get('/', (req, res) => {
+  console.log('GET');
+  res.send('Hello World!');
+});
 
 /** get a single book by id. */
-app.get('/:id', (req, res) => {
-    console.log("GET")
-    //how to get id?
-    res.send('Hello World!')
-})
+bookRouter.get('/:id', (req, res) => {
+  console.log('GET');
+  //how to get id?
+  res.send('Hello World!');
+});
 
 /** search multiple by title */
-app.get('/s/:search', (req, res) => {
-    console.log("GET")
-    res.send('Hello World!')
-})
+bookRouter.get('/s/:search', (req, res) => {
+  console.log('GET');
+  res.send('Hello World!');
+});
 
 /** TODO the update-route */
-app.put('/', (req, res) => {
-    console.log("PUT")
-    res.send('Got a PUT request at /user')
-})
+bookRouter.put('/', (req, res) => {
+  console.log('PUT');
+  res.send('Got a PUT request at /user');
+});
 
 /** TODO the delete-route */
-app.delete('/', (req, res) => {
-    console.log("DELETE")
-    res.send('Got a DELETE request at /user')
-})
+bookRouter.delete('/', (req, res) => {
+  console.log('DELETE');
+  res.send('Got a DELETE request at /user');
+});
+
+module.exports = bookRouter;
