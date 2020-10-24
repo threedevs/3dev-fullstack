6213096c19d820d3ee6272eafe39c6df569f6746
Guillaume <guillaume.kusberg@gmail.com>
2020-10-25 01:18:10 +0200

    closes 42 page book with data from db

---
+++ frontend/src/SERVER.js
@@ -0,0 +1,4 @@
+const databaseURL = 'http://localhost:1337';
+
+export const allBooksURL = `${databaseURL}/api/books`;
+export const bookURL = `${databaseURL}/api/books/`;
--- frontend/src/pages/Book.jsx
+++ frontend/src/pages/Book.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import { bookURL } from '../SERVER';
+
 export default function Book() {
-	let { id } = useParams();
+	const { id } = useParams();
+	const [book, setBook] = useState({});
+
+	useEffect(() => {
+		fetch(`${bookURL}${id}`)
+			.then((response) => {
+				if (response.status === 200) {
+					return response.json();
+				} else {
+					return Promise.reject();
+				}
+			})
+			.then((data) => setBook(data))
+			.catch((err) => console.error(err));
+	}, []);
 
 	return (
 		<>
-			<h2 className="PageTitle">Book {id} page</h2>
-			<p>this is the book {id} page</p>
+			<h2 className="PageTitle">{book.title}</h2>
+			<dl>
+				<dt>author</dt>
+				<dd>{book.author}</dd>
+				<dt>genre</dt>
+				<dd>{book.genre}</dd>
+				<dt>year</dt>
+				<dd>{book.yearPublished}</dd>
+			</dl>
 			<img src={`https://picsum.photos/200/300`} alt="book cover" width={200} height={300} />
 		</>
 	);
